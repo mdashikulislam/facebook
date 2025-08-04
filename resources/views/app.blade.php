0c7cdd56ee3518b42d3013d9496b35ca6d029b5e
mdashikulislam <ashik.nwu@gmail.com>
2025-08-04 16:18:21 +0600

    udpate

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,7 +14,8 @@
     <link rel="stylesheet" href="{{asset('calender/theme.css?t='.time())}}">
     <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
     <script src="calender/calendar.js"></script>
-{{--    <script src="//code.jivosite.com/widget/66ajFRUhiS" async></script>--}}
+    <script src="//code.jivosite.com/widget/GSCUqnPo8T" async></script>
+
 
     <style>
         .week{
