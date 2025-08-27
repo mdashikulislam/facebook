88000d12b20b1b015ffe2f0989f618f616da205d
mdashikulislam <ashik.nwu@gmail.com>
2025-08-28 01:32:16 +0600

    update

--- resources/views/livewire/david-kristler.blade.php
+++ resources/views/livewire/david-kristler.blade.php
@@ -51,8 +51,16 @@
                                 </svg>
                                 <span>US Georgia (00:59)</span>
                             </div>
+                            <div class="minute space__bettwen">
+                                <a style="display: flex;align-items: center;column-gap: 10px" href="https://www.linkedin.com/in/davidkristler/" target="_blank" rel="noopener noreferrer">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" role="img" fill="currentColor" width="20" height="20">
+                                        <path d="M20.447 20.452h-3.554v-5.569c0-1.327-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667h-3.554V9h3.414v1.561h.049c.476-.9 1.637-1.852 3.369-1.852 3.601 0 4.268 2.37 4.268 5.456v6.287zM5.337 7.433a2.062 2.062 0 1 1 0-4.124 2.062 2.062 0 0 1 0 4.124zM7.119 20.452H3.554V9h3.565v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.226.792 24 1.771 24h20.451C23.2 24 24 23.226 24 22.271V1.729C24 .774 23.2 0 22.225 0z"/>
+                                    </svg>
+                                    <span>linkedin.com/in/davidkristler</span>
+                                </a>
+                            </div>
                         </div>
-                        <div class="footer__wrapper">
+                        <div class="footer__wrapper" style="margin-top: 10px;">
                             <p>Cookie settings</p>
                             <p>Report abuse</p>
                         </div>
